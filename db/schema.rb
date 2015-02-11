527dbef2247f3f96b9490da83b57c8560342318c
Dorian Iacobescu <dorian.iacobescu@gmail.com>
2015-02-11 18:22:22 +0200

    Generated Books Scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150210173744) do
+ActiveRecord::Schema.define(version: 20150211155319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: true do |t|
+    t.string   "name"
+    t.string   "author"
+    t.text     "description"
+    t.integer  "price"
+    t.boolean  "availability", default: true
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
