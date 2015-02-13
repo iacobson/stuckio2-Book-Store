aaee63e3e24a6242f76f5aa8f96c5fbad5976ac0
Dorian Iacobescu <dorian.iacobescu@gmail.com>
2015-02-13 16:50:33 +0200

    Created the Dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :books # this will enable "@book = current_user.books.new(book_params)" method
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150211155319) do
+ActiveRecord::Schema.define(version: 20150213131941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20150211155319) do
     t.boolean  "availability", default: true
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
